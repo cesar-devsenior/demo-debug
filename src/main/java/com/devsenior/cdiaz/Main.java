408a7aa68ddefcaf0f3ba53d159503daa7002a2b
Cesar Augusto Diaz Arriaga <cesardevsenior@gmail.com>
2025-01-29 22:47:55 -0500

    Ajuste a los ejemplos

--- src/main/java/com/devsenior/cdiaz/Main.java
+++ src/main/java/com/devsenior/cdiaz/Main.java
@@ -5,10 +5,13 @@ public class Main {
         var inventory = new Inventory();
         inventory.addProduct(new Product("Laptop", 10, 3000));
         inventory.addProduct(new Product("Phone", 0, 600));
+        inventory.addProduct(new Product("laptop", 5, 1500));
 
         inventory.sellProduct("laptop", 5);
         inventory.sellProduct("phone", 1);
         inventory.sellProduct("tablet", 2);
+        inventory.sellProduct("LAPTOP", 6);
+
 
         var total = inventory.calculateTotalInventory();
         System.out.printf("Valor total del inventaro es: $ %.2f%n", total);
